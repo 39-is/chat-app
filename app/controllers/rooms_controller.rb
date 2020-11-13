d2802fee6c76608032ba59ac45e01a7a613fa268
issei fujii <f.0049w@gmail.com>
2020-11-14 01:43:17 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,9 @@
 class RoomsController < ApplicationController
+
+  def index
+  end
+
+
   def new
     @room = Room.new
   end
@@ -17,4 +22,6 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
+
+  
 end
